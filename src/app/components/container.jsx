5b5f6fa381e3a8bd94a5fda7676841e1b42570eb
MAS <mhmwdbas310@gmail.com>
2025-10-31 20:27:36 +0300

    update somthing 3

--- src/app/components/container.jsx
+++ src/app/components/container.jsx
@@ -1,6 +1,6 @@
 const Container = ({ children }) => {
   return (
-    <div className="relative  px-1 w-full h-full max-w-[500px] mx-auto   ">
+    <div className="relative  px-2 w-full h-full max-w-[500px] mx-auto   ">
       {children}
     </div>
   );
